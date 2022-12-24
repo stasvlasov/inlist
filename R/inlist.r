9f735dcbdb0ae67b4a29904d06cb5bc37b174ad5
Stas Vlasov <stasvlasov@users.noreply.github.com>
2022-12-24 14:18:14 +0100

    Deploy as R package

--- R/inlist.r
+++ R/inlist.r
@@ -12,14 +12,17 @@ inlist <- function(l, extractor, selector) {
     print(sys_call)
     parent_frame <- parent.frame()
     ## eval in elements envir
-    .eval <- \(envir, index, expr, call, fallback) {
+    .eval <- \(envir, index, expr, call, fallback, .l = l, n = index) {
         ## `(` is identity function
         ## prepend dot to names
         names(envir) <- ifelse(names(envir) != "", paste0(".", names(envir)), "")
         envir <- c(envir, list(. = envir
-                             , .. = l
+                             , .. = .l
                              , .n = index
-                             , .N = length(l)))
+                             , .N = length(.l)
+                             , ... = l
+                             , ..n = n
+                             , ..N = length(l)))
         vars <- all.vars(expr)
         ## find vars names that starts with .
         vars <- vars[substr(vars,0,1) == "."]
@@ -31,10 +34,11 @@ inlist <- function(l, extractor, selector) {
         }
     }
     ## apply with index
-    .apply <- \(.l, expr, call = `(`, fallback = NULL, simplify = FALSE) {
-        mapply(\(e, i) .eval(e, i, expr, call, fallback)
+    .apply <- \(.l, expr, call = `(`, fallback = NULL, simplify = FALSE, ..n = NULL) {
+        mapply(\(e, i, n) .eval(e, i, expr, call, fallback, .l, n)
              , e = .l
              , i = seq_along(.l)
+             , n = if(is.null(..n)) seq_along(.l) else ..n
              , SIMPLIFY = simplify)
     }
     lapply
@@ -45,7 +49,7 @@ inlist <- function(l, extractor, selector) {
     extract_l <- .apply(l, extractor, call = isTRUE, fallback = FALSE, simplify = TRUE)
     ## map
     selector <- sys_call[[4]]
-    select_l <- .apply(l[extract_l], selector, call = list)
+    select_l <- .apply(l[extract_l], selector, call = list, ..n = seq_along(l)[extract_l])
     ## return()
     Filter(Negate(is.null), select_l) |>
         lapply(unlist, recursive = FALSE)
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 
 
+The `inlist` package provides presumably more convinient version of 'apply' for lists with table like structure via its `inlist` function. Each list's element is bound to enviroments where filtering and selection is evaluated so its named elements are available as variables prefixed with `.`. It also binds `.`, `..`, `...`, `.n`, `.N`, `..n` and `..N` variables for accessing element itself, original list, elements indexing, length, etc.
+
+
 # Installation
 
     devtools::install_github("stasvlasov/inlist")
